a0550cc78eb18a9d675043c3791930f176225656
Gopinath V <gopinath.v.16@mountblue.tech>
2021-06-22 18:46:32 +0530

    added .flake8 file

--- student/views.py
+++ student/views.py
@@ -21,7 +21,6 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            useranme = form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created! You are\
                                         now able to log in')
             return redirect('login')
